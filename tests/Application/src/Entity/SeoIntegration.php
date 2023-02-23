e1715a448ff56e6d0feeff5600a3ace1bc186b4e
abartoszek <andrzej.bartoszek@lemisoft.pl>
2023-02-23 14:38:30 +0100

    konfiguracja testowej aplikacji

---
+++ tests/Application/src/Entity/SeoIntegration.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lemisoft\Tests\SyliusSeoIntegrationPlugin\Application\src\Entity;
+
+use Lemisoft\SyliusSeoIntegrationPlugin\Entity\Seo\SeoIntegration as BaseSeoIntegration;
+
+class SeoIntegration extends BaseSeoIntegration
+{
+
+}
